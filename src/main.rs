62bdfd42aa41b80a5eeda8641f508584ddb751b8
Kevin Zumueller <kevin@zumueller.com>
2021-01-12 23:46:41 +0100

    added SQL statements for testing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
-extern crate mysql;
+//extern crate mysql;
 
 use mysql::*;
+use mysql::prelude::*;
+
+struct Entry {
+    name: String,
+    value: f64,
+    time: u64,
+}
+
 
 fn main() {
-    println!("trying to establish connection");
+    let pool = Pool::new("mysql://default@localhost:9004/main").unwrap();
+    let mut connection = pool.get_conn().unwrap();
 
-    let pool = Pool::new("mysql://default@localhost:9004/main");
+    connection.query_drop("CREATE TABLE IF NOT EXISTS `test` (
+        `name` String,
+        `value` Float64,
+        `time` DateTime64(3)
+    ) ENGINE=Log").unwrap();
 
-    println!("Connection established!");
+    let entries = vec![
+        Entry{name: "AAPL".into(), value: 128.92, time: 1610489275855},
+        Entry{name: "AAPL".into(), value: 129.68, time: 1610489275915},
+        Entry{name: "AAPL".into(), value: 129.89, time: 1610489275975},
+    ];
+
+    connection.exec_batch("INSERT INTO `test`
+        (*)
+        VALUES (:name, :value, :time)",
+        entries.iter().map(|entry| params! {
+            ":name" => &entry.name,
+            ":value" => entry.value,
+            ":time" => entry.time
+        })
+    ).unwrap();
+
+    let sorted = connection.query_map("SELECT * FROM `test` ORDER BY `value` DESC", |(name, value, time)| {
+        Entry {name, value, time}
+    }).unwrap();
+
+
+    println!("highestValue: {} {} {}", sorted[0].value, sorted[0].name, sorted[0].time);
 }
